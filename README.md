f9d54116df5922044898504535cb177283de54f7
nicolas aguirre <nicolas.aguirre@utp.edu.co>
2021-06-17 19:00:58 -0500

    Complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DataVisualizationPractica-
 Es un repositorio creado con el fin de mostrar el código fuente solicitado para la plaza de Estado Joven 2021 
+
+Este repositorio consta de tres archivos, el principal index.html que se utiliza como página predeterminada para la visualización de la Base de Datos, por otro lado está el archivo styles.css que define los estilos que se utilizarán (tipos de letra, tamaños, etc), por ultimo esta el archivo script.js que es el encargado de las funcionalidades y el consumo de la Base de Datos a partir de un archivo .JSON proporcionado de manera gratuita por la organización FreeCodeCamp.
+
---
+++ script.js
@@ -0,0 +1,93 @@
+
+// Se consume los archivos de formatos JSON del sitio web FreeCodeCamp que almacenan
+// las Base de Datos que se visualizarán en este código haciendo uso de las funcionalidades
+// proporcionadas por la librería de JavaScript d3 
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyData
+let educationData
+
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
+
+// Esta función se encarga de realizar el diagramado del mapa  
+// así como el consumo de la BD para realizar el tratado de los datos
+// que se requiere para su correcta visualización
+let drawMap = () => {
+ 
+    canvas.selectAll('path')
+            .data(countyData)
+            .enter()
+            .append('path')
+            .attr('d', d3.geoPath())
+            .attr('class', 'county')
+            .attr('fill', (countyDataItem) => {
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
+                let percentage = county['bachelorsOrHigher']
+                if(percentage <= 15){
+                    return 'tomato'
+                }else if(percentage <= 30){
+                    return 'orange'
+                }else if(percentage <= 45){
+                    return 'lightgreen'
+                }else{
+                    return 'limegreen'
+                }
+            })
+            .attr('data-fips', (countyDataItem) => {
+                return countyDataItem['id']
+            })
+            .attr('data-education', (countyDataItem) => {
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
+                let percentage = county['bachelorsOrHigher']
+                return percentage
+            })
+            .on('mouseover', (countyDataItem)=> {
+                tooltip.transition()
+                    .style('visibility', 'visible')
+
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
+
+                tooltip.text(county['fips'] + ' - ' + county['area_name'] + ', ' + 
+                    county['state'] + ' : ' + county['bachelorsOrHigher'] + '%')
+
+                tooltip.attr('data-education', county['bachelorsOrHigher'] )
+            })
+            .on('mouseout', (countyDataItem) => {
+                tooltip.transition()
+                    .style('visibility', 'hidden')
+            })
+}
+
+d3.json(countyURL).then(
+    (data, error) => {
+        if(error){
+            console.log(log)
+        }else{
+            countyData = topojson.feature(data, data.objects.counties).features
+            console.log(countyData)
+
+            d3.json(educationURL).then(
+                (data, error) => {
+                    if(error){
+                        console.log(error)
+                    }else{
+                        educationData = data
+                        console.log(educationData)
+                        drawMap()
+                    }
+                }
+            )
+        }
+    }
+)
